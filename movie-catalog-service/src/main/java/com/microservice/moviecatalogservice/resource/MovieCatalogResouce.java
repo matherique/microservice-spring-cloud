28bcf980800a820fecd161cce2a9ada72ff12397
Matheus Henrique <matheus.henrique@seidor.com.br>
2021-05-10 21:34:24 -0300

    add movice catalog endpoint

--- movie-catalog-service/src/main/java/com/microservice/moviecatalogservice/resource/MovieCatalogResouce.java
+++ movie-catalog-service/src/main/java/com/microservice/moviecatalogservice/resource/MovieCatalogResouce.java
@@ -1,10 +1,22 @@
 package com.microservice.moviecatalogservice.resource;
 
+import com.microservice.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResouce {
-  
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "teste", 4)
+        );
+    }
 }
